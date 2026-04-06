1fb10b8cab788ff5661de216a85f593dd63038d2
kefarova-png <kefarova@gmail.com>
2026-04-07 02:47:32 +0400

    добавлена функция add_book(library, title, author, year) с оптимизацией

--- dict_of_books.py
+++ dict_of_books.py
@@ -8,52 +8,50 @@ def book_list_view(library):
         print("\t", book)  #  Вывод каждого названия
 
 
-def add_book(title, author, year):
-    if title not in library:
-        # Добавляем новую книгу
-        library[title] = {
-            "автор": author,
-            "год_издания": year,
-            "наличие": None
-        }
-        print(f"\nКнига '{title}' ({author}, {year}) успешно добавлена.")
-    else:
-        # Если книга с таким title уже есть, предлагаем обновить или оставить без изменений
-        choice = ''
+def add_book(library, title, author, year):
+    choice = ''  #  параметр решения об обновлении
+    if title in library:
         while choice != 'да' and choice != 'нет':
             print(f"\nКнига '{title}' уже существует.")
             choice = input(f"Хотите обновить информацию о книге '{title}'? (да/нет): ").strip().lower()
+            if choice == 'нет':
+                print(f"Обновление записи о книге '{title}' отменено.")
+                return
             if choice == 'да':
-                book_info = library[title]  #  получаеи словарь с информацией о книге с таким title
-                # Обновляем словарь с информацией о книге
-                book_info['автор'] = author
-                book_info['год_издания'] = year
-                library[title] = book_info  #  возвращаем/обновляем словарь в library
-                print(f"Информация о книге '{title}' обновлена:")
-                print(f"Книга '{title}' ({author}, {year})")
-            elif choice == 'нет':
-                print("Обновление отменено.")
+                book_aсtion = 'Обновление'
+                book_info = library[title]  #  получаем словарь книги title для обновления
             else:
                 print("Ответ не принят. Пожалуста, в следующий раз введите или ДА, или НЕТ.")
+    else:
+        book_aсtion = 'Добавление'
+        book_info = {}  #  создаём словарь книги title при добавлении
+        book_info['наличие'] = None
+    book_info['автор'] = author
+    book_info['год_издания'] = year
+    library[title] = book_info  #  Обновляем/добавляем словарь книги title в library
+    print(f"\n{book_aсtion} записи о книге '{title}' завершено:")
+    print(f"Книга '{title}' ({author}, {year})")
 
 
 library = {
     "Война и мир": {
         "автор": "Лев Толстой",
         "год_издания": 1869,
-        "наличие": "в наличии"
+        "наличие": "True"
     },
     "Мастера и Маргарита": {
         "автор": "Михаил Булгаков",
         "год_издания": 1967,
-        "наличие": "выдана"
+        "наличие": "False"
     }
 }
-
-
 book_list_view(library)
-(title, author, year) = ("Война и мир", "Лев Толстой", 1980)
-add_book(title, author, year)
-(title, author, year) = ("Лолита", "Владимир Навбоков", 1999)
-add_book(title, author, year)
-print("\n", library)
+title = "Война и мир"
+author = "Лев Толстой"
+year = 1980
+add_book(library, title, author, year)
+title = "Лолита"
+author = "Владимир Набоков"
+year = 1999
+add_book(library, title, author, year)
+print('\n', library)  #  для контроля происходящего
