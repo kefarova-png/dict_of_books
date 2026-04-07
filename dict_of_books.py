b5869aa3f47bfbd963f8934e5e7cc908baa80d37
kefarova-png <kefarova@gmail.com>
2026-04-07 18:59:25 +0400

    Внесены корректировки, оптимизации

--- dict_of_books.py
+++ dict_of_books.py
@@ -11,25 +11,23 @@ def book_list_view(library):
 def add_book(library, title, author, year):
     choice = ''  #  параметр решения об обновлении
     if title in library:
+        print(f"\nКнига '{title}' уже существует.")
         while choice != 'да' and choice != 'нет':
-            print(f"\nКнига '{title}' уже существует.")
             choice = input(f"Хотите обновить информацию о книге '{title}'? (да/нет): ").strip().lower()
             if choice == 'нет':
                 print(f"Обновление записи о книге '{title}' отменено.")
                 return
             if choice == 'да':
-                book_aсtion = 'Обновление'
                 book_info = library[title]  #  получаем словарь книги title для обновления
             else:
                 print("Ответ не принят. Пожалуста, в следующий раз введите или ДА, или НЕТ.")
+        book_action = 'Обновление'
+        presence = library[title]['наличие']
     else:
-        book_aсtion = 'Добавление'
-        book_info = {}  #  создаём словарь книги title при добавлении
-        book_info['наличие'] = None
-    book_info['автор'] = author
-    book_info['год_издания'] = year
-    library[title] = book_info  #  Обновляем/добавляем словарь книги title в library
-    print(f"\n{book_aсtion} записи о книге '{title}' завершено:")
+        book_action = 'Добавление'
+        presence = "None"
+    library[title] = {"автор": author, "год_издания": year, "наличие": presence}
+    print(f"\n{book_action} записи о книге '{title}' завершено:")
     print(f"Книга '{title}' ({author}, {year})")
 
 
