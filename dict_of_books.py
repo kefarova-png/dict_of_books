d140a86bc9aa4d1d27d247c5b996e011e7633c6c
kefarova-png <kefarova@gmail.com>
2026-04-08 18:17:45 +0400

    Созданы функции изменения статуса книг.

--- dict_of_books.py
+++ dict_of_books.py
@@ -8,6 +8,7 @@ def book_list_view(library):
         print("\t", book)  #  Вывод каждого названия
 
 
+#  Добавление/обновление книги
 def add_book(library, title, author, year):
     choice = ''  #  параметр решения об обновлении
     if title in library:
@@ -28,13 +29,38 @@ def add_book(library, title, author, year):
     print(f"\n{book_action} записи о книге '{title}' завершено:")
     print(f"Книга '{title}' ({author}, {year})")
 
- #  Удаление книги из библиотеки
+
+#  Удаление книги из библиотеки
 def remove_book(library, title):
     if title not in library:
-        print(f"\nКнига '{title}'отсутствует в библиотеке.")
+        print(f"\nКнига '{title}'отсутствует в списке книг библиотеки.")
     else:
         del library[title]
-        print(f"\nКнига '{title}' успешно удалена из библиотеки.")
+        print(f"\nКнига '{title}' успешно удалена из списка книг библиотеки.")
+
+
+#  Выдача книги
+def issue_book(library, title):
+    if title not in library:
+        print(f"\nКнига '{title}' отсутствует в списке книг библиотеки.")
+        return
+    if library[title]['наличие'] == "False":
+        print(f"\nОшибка! Книга '{title}' уже отмечена как выданная.")
+    else:
+        library[title]['наличие'] = "False"
+        print(f"\nКнига '{title}' отмечена как выданная.")
+
+
+#  Возврат книги
+def return_book(library, title):
+    if title not in library:
+        print(f"\nКнига '{title}' отсутствует в списке книг библиотеки.")
+        return
+    if library[title]['наличие'] == "True":
+        print(f"\nОшибка! Книга '{title}' уже отмечена как вернувшаяся в библиотеку.")
+    else:
+        library[title]['наличие'] = "True"
+        print(f"\nКнига '{title}' отмечена как вернувшаяся в библиотеку.")
 
 
 library = {
@@ -54,15 +80,28 @@ library = {
 # author = "Лев Толстой"
 # year = 1980
 # add_book(library, title, author, year)
-# title = "Лолита"
-# author = "Владимир Набоков"
-# year = 1999
-# add_book(library, title, author, year)
+title = "Лолита"
+author = "Владимир Набоков"
+year = 1999
+add_book(library, title, author, year)
 # print('\n', library)  #  для контроля происходящего
-title = 'Война и мир'
-remove_book(library, title)
-print("\n", library)  #  для контроля происходящего
-title = 'Война и мир'
-remove_book(library, title)
-print("\n", library)  #  для контроля происходящего
 
+# title = 'Война и мир'
+# remove_book(library, title)
+# print("\n", library)  #  для контроля происходящего
+# title = 'Война и мир'
+# remove_book(library, title)
+# print("\n", library)  #  для контроля происходящего
+
+title = "QWERTY"
+issue_book(library, title)
+return_book(library, title)
+title = "Лолита"
+issue_book(library, title)
+title = "Война и мир"
+issue_book(library, title)
+issue_book(library, title)
+title = "Мастера и Маргарита"
+return_book(library, title)
+return_book(library, title)
+print("\n", library)  #  для контроля происходящего
